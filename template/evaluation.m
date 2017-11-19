798f24dce069807da9354d3effcdb61ad66078a0
Aasmund Brekke <aasmundbrekke@gmail.com>
2017-11-19 16:51:50 +0100

    Add option for choosing parameter to tweak

--- template/evaluation.m
+++ template/evaluation.m
@@ -11,8 +11,8 @@ PRECI=1;		% Precision of variables
 ELITIST=0.05;    % percentage of the elite population
 GGAP=1-ELITIST;		% Generation gap
 STOP_PERCENTAGE=.95;    % percentage of equal fitness individuals for stopping
-PR_CROSS=.95;     % probability of crossover
-PR_MUT=.05;       % probability of mutation
+PR_CROSS=0.95;     % probability of crossover
+PR_MUT=0.05;       % probability of mutation
 LOCALLOOP=0;      % local loop removal
 CROSSOVER = 'xalt_edges';  %crossover operator
 DATASET_PATH = 'datasets/'; %subfolder that contains the datasets
@@ -31,14 +31,19 @@ addpath('../gatbx');
 %mkdir(PLOT_PATH);
 
 
-NUM_PRS = 5; % Number of parameter values (linearly spaced between 0 and 1 for probabilities) 
-
+NUM_PRS = 10; % Number of parameter values (linearly spaced between 0 and 1 for probabilities) 
 PR_CROSSES = linspace(0, 1, NUM_PRS);
 PR_MUTS = linspace(0, 1, NUM_PRS);
-NUM_RUNS = 1; % Number of times we evaluate each parameter setting 
-data_names = {'rondrit016' 'rondrit048' 'rondrit070'}; %'rondrit100' 'rondrit127'};
+POP_SIZE = linspace(5, 2500, NUM_PRS);
+NUM_RUNS = 10; % Number of times we evaluate each parameter setting 
+data_names = {'rondrit016' 'rondrit048' 'rondrit070' 'rondrit100' 'rondrit127'};
 data_space = linspace(1, 3, length(data_names));
 PERFORMANCE = zeros(length(data_names),NUM_PRS);
+CHOSEN_PARAM = 'mutation'; 
+KEYSET = {'crossover', 'mutation', 'population'};
+VALUES = {PR_CROSSES, PR_MUTS, POP_SIZE};
+PARAMETER_MAP = containers.Map(KEYSET, VALUES);
+CHOSEN_PARAM_DATA = PARAMETER_MAP(CHOSEN_PARAM);
 
 
 % Variables to keep track of indices
@@ -53,11 +58,24 @@ for data_name = data_names
     NVAR=size(data,1);
     % TODO: Change this to a general method which can accept different
     % parameters (mutation, individuals, ..etc) and iterate through them
-    for PR_MUT = PR_MUTS
+    for PARAM = CHOSEN_PARAM_DATA
+        avg_dist = 0;
         for run = 1:NUM_RUNS
-            avg_dist = 0;
-            [path, dist, nr_gens, best_fits, mean_fits, worst_fits] = run_ga2(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
-            avg_dist = avg_dist + dist;
+            % We have to do these string checks to get the param in the
+            % correct argument for the run_ga function
+            if strcmp(CHOSEN_PARAM, 'crossover')
+                [path, dist, nr_gens, best_fits, mean_fits, worst_fits] = run_ga2(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PARAM, PR_MUT, CROSSOVER, LOCALLOOP);
+                avg_dist = avg_dist + dist;
+            elseif strcmp(CHOSEN_PARAM, 'mutation')
+                [path, dist, nr_gens, best_fits, mean_fits, worst_fits] = run_ga2(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PARAM, CROSSOVER, LOCALLOOP);
+                avg_dist = avg_dist + dist;
+            elseif strcmp(CHOSEN_PARAM, 'population')
+                [path, dist, nr_gens, best_fits, mean_fits, worst_fits] = run_ga2(x, y, PARAM, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
+                avg_dist = avg_dist + dist;
+            else
+                msg = 'Must specify either crossover, mutation or population as chosen parameter!';
+                error(msg)
+            end 
         end
         PERFORMANCE(data_num, pr_num) = avg_dist / NUM_RUNS;
         pr_num = pr_num + 1;
@@ -69,11 +87,10 @@ end
 [X,Y] = meshgrid(PR_CROSSES, data_space);
 %[X, Y, Z] = meshgrid(data, PR_CROSSES, P);
 for i = 1:length(data_names)
-    disp("Hello world");
     disp(size(PR_CROSSES))
     disp(size(repmat(i, length(data_names), 1)))
     disp(size(PERFORMANCE(i, :)))
-    plot3(PR_MUTS,repmat(i, length(PR_MUTS), 1),PERFORMANCE(i, :), '-o');
+    plot3(PR_CROSSES,repmat(i, length(PR_CROSSES), 1),PERFORMANCE(i, :), '-o');
     grid on;
     hold on;
 end
